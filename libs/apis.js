f2c7531072d65ca0d083150be94b7872ac3c051b
Guo Yu <i@guoyu.me>
2014-04-14 01:11:13 +0800

    0.0.1: add callback support

--- libs/apis.js
+++ libs/apis.js
@@ -1,8 +1,26 @@
+var errors = require('./errors');
+
 exports.fm = {
   auth: {
     method: 'post',
-    url: '/j/app/login'
+    url: '/j/app/login',
+    callback: function(err, res, result, next) {
+      if (result.r == 0) return next(null, result);
+      return next(errors[result.err]);
+    }
   },
-  songs: '/j/app/radio/people',
-  channels: '/j/app/radio/channels'
+  songs: {
+    url: '/j/app/radio/people',
+    callback: function(err, res, result, next) {
+      if (result.r == 0) return next(null, result.song, result);
+      return next(result.err);
+    }
+  },
+  channels: {
+    url:'/j/app/radio/channels',
+    callback: function(err, res, result, next) {
+      if (!result.channels) return next(new Error(result.err));
+      return next(null, result.channels);
+    }
+  }
 }
